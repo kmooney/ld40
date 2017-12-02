d2cfdc45bad031c1e716f52f0fa88af05e4ad106
kmooney <kmooney@gmail.com>
2017-12-02 10:55:54 -0600

    add seconds to timestamp in console

--- src/console.js
+++ src/console.js
@@ -5,7 +5,7 @@ window.ShipsLog =
     "use strict";
     var americanDateTime = new Intl.DateTimeFormat('en-US', {
         year: '2-digit', month: '2-digit', day: '2-digit',
-        hour: '2-digit', minute: '2-digit', timeZoneName: 'short' }
+        hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' }
     ).format;
 
     var container = document.getElementById('container');
